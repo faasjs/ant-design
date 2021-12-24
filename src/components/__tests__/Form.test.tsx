e60c14e079018d5e60bdccc5eabf4d2171a994ee
Ben <benz303bb@gmail.com>
2021-12-24 11:28:17 +0800

    fix case

---
+++ src/components/__tests__/Form.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { Form } from '../Form'
+
+describe('FormItem', () => {
+  it('should work', function () {
+    render(<Form
+      items={ [{ id: 'test' }] } />)
+
+    expect(screen.getByText('Test')).toBeInTheDocument()
+  })
+})
